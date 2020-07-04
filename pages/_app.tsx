4f31abe40577e8331d16af1935ebfd2bc3ccc278
Bùi Ngọc Thơ <thobn24h@gmail.com>
2020-07-04 13:21:39 +0700

    init project with custom ant css

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import '../css/antd.less';
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <Component {...pageProps} />
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import headerStyled from '../components/Header.module.css'
+
+export default () => {
+  return (
+    <>
+      <div className={headerStyled.appc}>
+        Hello World
+      </div>
+    </>
+  )
+}
